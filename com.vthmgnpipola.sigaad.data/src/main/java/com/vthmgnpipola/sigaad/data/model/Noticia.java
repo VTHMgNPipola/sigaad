2601d42445ad06761bd79068a747034dd80f0ade
VTHMgNPipola <web.anunciante@gmail.com>
2021-10-17 15:06:54 -0300

    Adicionado modelo de dados para as notícias.

---
+++ com.vthmgnpipola.sigaad.data/src/main/java/com/vthmgnpipola/sigaad/data/model/Noticia.java
@@ -0,0 +1,60 @@
+/*
+ * sigaad.data
+ * Copyright (C) 2021  VTHMgNPipola
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.vthmgnpipola.sigaad.data.model;
+
+import java.time.LocalDateTime;
+
+public class Noticia {
+    private String titulo;
+    private LocalDateTime dataCriacao;
+    private String mensagem;
+    private String autor;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
